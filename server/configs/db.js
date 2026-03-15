48161b83b3cf388acd7a1a3ba993b62d70ee3e70
Vikas Kr. Yadav <121151379+vip9569@users.noreply.github.com>
2026-03-15 11:02:05 +0530

    Update MongoDB connection URI in db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () =>
       console.log("Database connected")
     );
-    await mongoose.connect(`${process.env.MONGO_URI}/grocerybee`);
+    console.log()
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.error(error.message);
   }
