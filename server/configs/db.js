d629f5db444a05ef63043b97c605d0be7c1861fe
Vikas Kr. Yadav <121151379+vip9569@users.noreply.github.com>
2026-03-15 11:05:46 +0530

    Log MONGO_URI during database connection

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () =>
       console.log("Database connected")
     );
-    console.log()
+    console.log(process.env.MONGO_URI)
     await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.error(error.message);
